77fe7a9f5253295a5be7aaa47b1a77acc45675b1
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2022-11-28 21:35:20 +0530

    Add threads.py

---
+++ python-sleep/threads.py
@@ -0,0 +1,27 @@
+import threading
+import time
+
+def func1():
+    for i in range(10):
+        print(f"Running t1, print {i}.")
+        time.sleep(2)
+
+def func2():
+    for i  in range(5):
+         print(f"Running t2, print {i}.")
+         time.sleep(1)
+
+
+def func3():
+    for i in range(4):
+         print(f"Running t3, print {i}.")
+         time.sleep(0.5)
+
+
+t1 = threading.Thread(target=func1)
+t2 = threading.Thread(target=func2)
+t3 = threading.Thread(target=func3)
+
+t1.start()
+t2.start()
+t3.start()
