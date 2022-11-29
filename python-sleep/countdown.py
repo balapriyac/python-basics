875546d1c99fd4f18672378706d355e7b5618d94
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2022-11-29 09:20:48 +0530

    Update countdown.py

--- python-sleep/countdown.py
+++ python-sleep/countdown.py
@@ -1,8 +1,11 @@
+import time
+
 def count_down(n):
     for i in range(n,-1,-1):
         if i==0:
             print("Ready to go!")
         else:
              print(i)
+             time.sleep(1)
 
 count_down(5)
