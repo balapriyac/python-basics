fbc3f06d757471cb4dc3852939d1811f5c251e81
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-03-19 08:30:07 +0530

    Update main.py

--- dict-to-json/main.py
+++ dict-to-json/main.py
@@ -1,1 +1,27 @@
+# Python dict to JSON
+import json
 
+books = [
+    {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "publication_year": 1925,
+        "genre": "Fiction"
+    },
+    {
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "publication_year": 1960,
+        "genre": "Fiction"
+    },
+    {
+        "title": "1984",
+        "author": "George Orwell",
+        "publication_year": 1949,
+        "genre": "Fiction"
+    }
+]
+
+# Convert dictionary to JSON string
+json_string = json.dumps(books, indent=4)
+print(json_string)
