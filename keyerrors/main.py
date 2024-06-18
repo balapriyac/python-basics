5e95a5d86c9f94032f885e08db26a5f697ae13ea
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-06-18 08:08:04 +0530

    Update main.py

--- keyerrors/main.py
+++ keyerrors/main.py
@@ -20,3 +20,10 @@ author = books.get("Brave New World", "Book not found")
 print(author) 
 
 
+# use defaultdict to handle missing keys natively
+from collections import defaultdict
+
+books_default = defaultdict(lambda: "Book not found",books)  
+# Access the key "Brave New World"
+author = books_default["Brave New World"]  
+print(author) 
