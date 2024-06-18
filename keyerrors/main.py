ca285a4512a892cd59abbe3009c35e28fb382740
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-06-18 08:06:20 +0530

    Update main.py

--- keyerrors/main.py
+++ keyerrors/main.py
@@ -1,1 +1,22 @@
+books = {
+	"1984": "George Orwell",
+	"To Kill a Mockingbird": "Harper Lee",
+	"The Great Gatsby": "F. Scott Fitzgerald"
+}
+
+
+# handle KeyError explicitly
+try:
+    # Try to access the key "Brave New World"
+	author = books["Brave New World"]  
+    # Catch the KeyError if the key does not exist
+except KeyError:  
+	author = "Book not found"  
+
+print(author) 
+
+# Try to get the value for "Brave New World"
+author = books.get("Brave New World", "Book not found")  
+print(author) 
+
 
