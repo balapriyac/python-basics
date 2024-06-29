7ae8b2b698dc6a2ef821468d2101dcc47dd340c6
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-06-29 14:34:40 +0530

    Update main.py

--- pathlib-tutorial/main.py
+++ pathlib-tutorial/main.py
@@ -1,1 +1,19 @@
+from pathlib import Path
+
+# import Path from pathlib
+from pathlib import Path
+
+# create a base path
+# replace user w/ a valid user :)
+base_path = Path("/home/user/Documents")
+
+# create new paths from the base path
+subdirectory_path = base_path / "projects" / "project1"
+file_path = subdirectory_path / "report.txt"
+
+# Print out the paths
+print("Base path:", base_path)
+print("Subdirectory path:", subdirectory_path)
+print("File path:", file_path)
+
 
