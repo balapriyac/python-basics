59b2af3c2d1e2ac4f08482c44d5c879e4f084a22
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-07-22 12:33:44 +0530

    Update main.py

--- regex/main.py
+++ regex/main.py
@@ -1,1 +1,13 @@
+import re
 
+text = "Contact info: (123)-456-7890 and 987-654-3210."
+cleaned_text = re.sub(r'[()-]', '', text)
+print(cleaned_text) 
+
+# Output: Contact info: 1234567890 or 9876543210
+
+text = "Please reach out to us at support@example.org or help@example.org."
+emails = re.findall(r'\b[\w.-]+?@\w+?\.\w+?\b', text)
+print(emails)
+
+# Output: ['support@example.com', 'sales@example.org']
