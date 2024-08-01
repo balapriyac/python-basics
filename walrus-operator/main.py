4236f214696e7e92895c8ba8f3c39127bc53d629
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-08-01 13:08:52 +0530

    Add example 2

--- walrus-operator/main.py
+++ walrus-operator/main.py
@@ -1,11 +1,31 @@
-# example 1
-# without walrus operator
+# Example 1
+# Without walrus operator
 data = input("Enter your data: ")
 while len(data) > 0:
     print("You entered:", data)
     data = input("Enter your data: ")
 
-# with walrus operator
+# With walrus operator
 while (data := input("Enter your data: ")) != "":
     print("You entered:", data)
 
+# Example 2
+# Without Walrus Operator
+# Function to compute profit
+def compute_profit(sales, cost):
+	return sales - cost
+
+sales_data = [(100, 70), (200, 150), (150, 100), (300, 200)]
+profits = []
+for sales, cost in sales_data:
+    profit = compute_profit(sales, cost)
+    if profit > 50:
+        profits.append(profit)
+
+# With Walrus Operator
+# Function to compute profit
+def compute_profit(sales, cost):
+	return sales - cost
+
+sales_data = [(100, 70), (200, 150), (150, 100), (300, 200)]
+profits = [profit for sales, cost in sales_data if (profit := compute_profit(sales, cost)) > 50]
