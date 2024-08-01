74ebdea092debc00d81fcd3a5230c389ce80ba50
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-08-01 13:07:28 +0530

    Add example 1

--- walrus-operator/main.py
+++ walrus-operator/main.py
@@ -1,1 +1,11 @@
+# example 1
+# without walrus operator
+data = input("Enter your data: ")
+while len(data) > 0:
+    print("You entered:", data)
+    data = input("Enter your data: ")
+
+# with walrus operator
+while (data := input("Enter your data: ")) != "":
+    print("You entered:", data)
 
