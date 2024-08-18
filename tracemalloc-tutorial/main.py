ca0a25ccc58ac7c77fe50ae4ca4d8310cd8c0b41
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-08-18 10:15:53 +0530

    Add main.py

--- tracemalloc-tutorial/main.py
+++ tracemalloc-tutorial/main.py
@@ -1,1 +1,13 @@
+# main.py
+import pandas as pd
 
+def load_data(file_path):
+    print("Loading data...")
+    df = pd.read_csv(file_path)
+    return df
+
+def process_data(df):
+    print("Processing data...")
+    df['DiscountedAmount'] = df['OrderAmount'] * 0.9  # Apply a 10% discount
+    df['OrderYear'] = pd.to_datetime(df['OrderDate']).dt.year  # Extract the order year
+    return df
