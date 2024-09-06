cf43601c49ed26ba682efc8904381644fe5fd1ce
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-09-06 11:07:12 +0530

    Create main.py

---
+++ merge-dict/main.py
@@ -0,0 +1,13 @@
+# sample dictionaries
+
+config1 = {
+	'database': 'Postgres',
+	'host': 'localhost',
+	'port': 5432
+}
+
+config2 = {
+	'username': 'admin',
+	'password': 'secret',
+	'timeout': 30
+}
