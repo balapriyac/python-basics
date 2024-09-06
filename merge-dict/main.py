fc69e28205ac2354cc36decfe83a57706f97ff1d
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2024-09-06 11:08:03 +0530

    Update main.py

--- merge-dict/main.py
+++ merge-dict/main.py
@@ -1,5 +1,4 @@
 # sample dictionaries
-
 config1 = {
 	'database': 'Postgres',
 	'host': 'localhost',
@@ -11,3 +10,20 @@ config2 = {
 	'password': 'secret',
 	'timeout': 30
 }
+
+# Merge using update() method
+final_config = config1.copy()  # Create a copy to avoid modifying the original config1
+final_config.update(config2)
+
+print(final_config)
+
+# Merge using dictionary unpacking
+final_config = {**config1, **config2}
+
+print(final_config)
+
+# Merge using | operator
+final_config = config1 | config2
+
+print(final_config)
+
