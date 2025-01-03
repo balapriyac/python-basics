d0772f84784ab85ee1792e24e9c534918e62cc66
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-01-03 12:26:22 +0530

    Update main.py

--- function-args/main.py
+++ function-args/main.py
@@ -1,1 +1,10 @@
+# Positional Arguments
+def meet(name,job_title,city):
+    return f"Meet {name}, a {job_title} from {city}."
 
+meet1 = meet('James','Writer','Nashville')
+print(meet1)
+
+# Keyword Arguments
+meet3 = meet(city='Madison',name='Ashley',job_title='Developer')
+print(meet3)
