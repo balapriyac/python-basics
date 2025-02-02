ad350aa1a5418191f05394f646939011c13280a3
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-02-02 19:06:37 +0530

    Add ologn_example.py

---
+++ big-o-examples/ologn_example.py
@@ -0,0 +1,12 @@
+def binary_search(sorted_arr, target):
+    left, right = 0, len(sorted_arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if sorted_arr[mid] == target:
+            return mid
+        elif sorted_arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
