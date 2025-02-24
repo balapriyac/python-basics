22acb0690372e42019a628353713e3b8e12ffb91
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-02-24 10:53:41 +0530

    Add total_ordering_dec.py

---
+++ useful-decorators/total_ordering_dec.py
@@ -0,0 +1,21 @@
+from functools import total_ordering
+
+@total_ordering
+class Version:
+    def __init__(self, major, minor, patch):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    def __eq__(self, other):
+        if not isinstance(other, Version):
+            return NotImplemented
+        return (self.major, self.minor, self.patch) == (other.major, other.minor, other.patch)
+
+    def __lt__(self, other):
+        if not isinstance(other, Version):
+            return NotImplemented
+        return (self.major, self.minor, self.patch) < (other.major, other.minor, other.patch)
+
+    def __repr__(self):
+        return f"v{self.major}.{self.minor}.{self.patch}"
