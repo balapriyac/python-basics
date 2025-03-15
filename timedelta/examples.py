d7952b4c3d85bda45bae79dfc9e52fb22e4341af
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-03-15 07:32:45 +0530

    Update examples.py

--- timedelta/examples.py
+++ timedelta/examples.py
@@ -1,1 +1,10 @@
+# Import Necessary Modules
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
 
+time_delt1 = timedelta(days= 270, hours = 9, minutes = 18)
+print(time_delt1)
+
+# Sample Output: 270 days, 9:18:00
