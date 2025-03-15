a8dee19b0ec3649d5dd87edc1a9d8a11622825fc
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-03-15 07:34:58 +0530

    Update examples.py

--- timedelta/examples.py
+++ timedelta/examples.py
@@ -20,3 +20,21 @@ print(future_date1)
 # What day will it be after 189 days
 future_date2 = time_now + timedelta(days=189)
 print(future_date2)
+
+# What day would it have been 189 days ago
+past_date1 = time_now - timedelta(days=189)
+print(past_date1)
+
+# create reference objects for today, and teachers' day
+teachers_day = date(time_now.year, 9, 5)
+today = date.today()
+
+# calculate number of days to teachers' day.
+time_to_td = teachers_day - today
+print(f"Teachers' day is {time_to_td.days} days away")
+
+# check if teachers' day has passed
+if teachers_day < today:
+  print(f"This year's Teachers' Day was {(today-teachers_day).days} days ago")
+  time_to_td = teachers_day - today
+  print(f"Next year's Teachers' day is {time_to_td.days} days away")
