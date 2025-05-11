1beeb5528ccaea12e6e700f5536149c52b62d7d1
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-05-11 21:12:20 +0530

    Update main.py

--- efficient-python-for-beginners/main.py
+++ efficient-python-for-beginners/main.py
@@ -1,1 +1,16 @@
+# instead of this
+def process_sales_data(sales):
+    highest_sale = sales[0]
+    for sale in sales:
+        if sale > highest_sale:
+            highest_sale = sale
+    
+    total_sales = 0
+    for sale in sales:
+        total_sales += sale
+    
+    return highest_sale, total_sales, total_sales / len(sales)
 
+# do this
+def process_sales_data(sales):
+    return max(sales), sum(sales), sum(sales) / len(sales)
