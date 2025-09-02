4ac08c1199967fad823eed12c4f3c409d97e0684
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-09-02 08:18:28 +0530

    Create generators.py

---
+++ generators-101/generators.py
@@ -0,0 +1,9 @@
+def get_cubes(num):
+    cubes = []
+    for i in range(num):
+        cubes.append(i**3)
+    return cubes
+
+def get_cubes(num):
+    cubes = [i**3 for i in range(num)]
+    return cubes
