968cc8cb2d84fd1cff567966d52c65550e445642
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-09-03 10:41:39 +0530

    Update tips.py

--- coding-interview-tips/tips.py
+++ coding-interview-tips/tips.py
@@ -1,1 +1,16 @@
+nums = [90,23,19,45,33,54]
+nums.reverse()
+print(nums)
 
+# Output >> [54, 33, 45, 19, 23, 90]
+
+nums = [23,67,12,78,94,113,47]
+nums.sort()
+print(nums)
+
+# Output >> [12, 23, 47, 67, 78, 94, 113]
+
+nums.sort(reverse=True)
+print(nums)
+
+# Output >> [113, 94, 78, 67, 47, 23, 12]
