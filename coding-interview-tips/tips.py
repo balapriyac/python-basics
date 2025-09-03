2c84f473e69433f75f68da2c72042c364422853a
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-09-03 10:47:17 +0530

    Update tips.py

--- coding-interview-tips/tips.py
+++ coding-interview-tips/tips.py
@@ -41,4 +41,8 @@ squares_dict = {i:i**2 for i in range(1,11)}
 print(squares_dict)
 
 # Output >> 
-{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100}
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100}
+
+strings = ["hello","coding","blue","work"]
+str_len = {string:len(string) for string in strings}
+print(str_len)
