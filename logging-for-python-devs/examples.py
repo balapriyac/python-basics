b39d661f1d0507d33a8464eea38502fe790fa708
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-12-23 10:11:13 +0530

    Update examples.py

--- logging-for-python-devs/examples.py
+++ logging-for-python-devs/examples.py
@@ -1,1 +1,27 @@
+import logging
 
+logger = logging.getLogger('my_app')
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+logger.debug('This is a debug message')
+logger.info('Application started')
+logger.warning('Disk space running low')
+logger.error('Failed to connect to database')
+logger.critical('System shutting down')
+
+###
